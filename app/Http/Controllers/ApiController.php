a02ebc95f87d5a9065de6ccca9e4dd6c05867128
Cybernetics <mail@cybernetics.me>
2025-02-20 12:38:50 +0530

    small updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
@@ -10,6 +11,9 @@ class ApiController extends Controller
     {
         $credentials = array('email' => $request->json('email'), 'password' => $request->json('password'));
         $user = null;
+        if (Auth::attempt($credentials)):
+            $user = Auth::getProvider()->retrieveByCredentials($credentials);
+        endif;
         if ($user):
             return response()->json([
                 'status' => true,
