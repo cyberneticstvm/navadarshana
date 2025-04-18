5bddef9cb49d1bbc7c2cc7463411af0b9b453ecc
Cybernetics <mail@cybernetics.me>
2025-04-18 09:34:11 +0530

    small updates

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -42,7 +42,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-12 ms-5">
+                            <div class="col-md-12 mx-5">
                                 {!! $note->notes !!}
                             </div>
                         </div>
