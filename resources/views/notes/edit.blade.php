88cf662e7a7e6829de78fab7f83c1a9f6391b4a7
Cybernetics <mail@cybernetics.me>
2025-04-18 09:14:35 +0530

    small updates

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -51,7 +51,7 @@
                             <div class="row mt-3">
                                 <div class="mb-3 col-md-12">
                                     <label class="form-label req">Note</label>
-                                    {{ html()->textarea('notes', $note->notes)->class("form-control")->attribute('id', 'editor1') }}
+                                    {{ html()->textarea('notes', $note->notes)->class("form-control")->attribute('id', 'editor') }}
                                 </div>
                                 @error('notes')
                                 <small class="text-danger">{{ $errors->first('notes') }}</small>
