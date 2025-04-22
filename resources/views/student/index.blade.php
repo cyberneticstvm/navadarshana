ce840997cbea6958b56f536d1f4a0d93a0b36f95
Cybernetics <mail@cybernetics.me>
2025-04-22 10:25:45 +0530

    small updates

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,7 +1,7 @@
 @extends("base")
 @section("content")
 <div class="content-body">
-    <div class="container">
+    <div class="container-fluid">
         <div class="page-titles">
             <h5 class="dashboard_bar">Student - <a href="{{ route('student.create') }}">Create New</a></h5>
             <ul class="breadcrumb">
