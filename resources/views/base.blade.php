9656a867c08ad851028e5f10b9eead4501fb5e09
Cybernetics <mail@cybernetics.me>
2025-04-23 09:39:47 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -61,7 +61,7 @@
         ***********************************-->
         <div class="nav-header">
             <a href="{{ route('dashboard') }}" class="brand-logo">
-                <img src="{{ asset('/assets/images/logo/pdf-logo.png') }}" width="50%" />
+                <img src="{{ asset('/assets/images/logo/pdf-logo.png') }}" />
             </a>
             <div class="nav-control">
                 <div class="hamburger">
