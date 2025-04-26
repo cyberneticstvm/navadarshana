cb1442305a27a67012a892c7ddc5ee965c3ce022
Cybernetics <mail@cybernetics.me>
2025-04-26 08:36:33 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -461,6 +461,7 @@
 									fontSize: '11px',
 									fontWeight: '500',
 									fontFamily: 'Arial',
+                                    color: (parseFloat(response['income']) - parseFloat(response['expense']) > 0) ? '#3AC977' : '#FF5E5E',
 									label: (parseFloat(response['income']) - parseFloat(response['expense']) > 0) ? 'Profit' : 'Loss',
 
 									/*formatter: function (w) {
