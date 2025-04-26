91c301cf4aefa9efa3519ee41893b9f4a0a1b8aa
Cybernetics <mail@cybernetics.me>
2025-04-26 08:30:48 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -468,7 +468,7 @@
 											return a + b
 										}, 0)
 									}*/
-                                    formatter: response['income'] - response['expense'],
+                                    formatter: parseFloat(response['income']) - parseFloat(response['expense']),
 								}
 							}
 						}
