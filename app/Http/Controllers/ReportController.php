b7bfb03e9c048131f402c39ff063c5fb8cd6b5bb
Cybernetics <mail@cybernetics.me>
2025-04-29 11:37:50 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -101,7 +101,7 @@ class ReportController extends Controller implements HasMiddleware
     function fee(Request $request)
     {
         $inputs = array(date('Y-m-d'), date('Y-m-d'), 'all', Session::get('branch'));
-        $category = array('admission' => 'Admission', 'monthly' => 'Batch', 'all' => 'All');
+        $category = array('admission' => 'Admission', 'monthly' => 'Batch', 'other' => 'Other', 'all' => 'All');
         $branches = $this->branches;
         $fees = Fee::where('branch_id', $inputs[3])->whereBetween('payment_date', [Carbon::parse($inputs[0])->startOfDay(), Carbon::parse($inputs[1])->endOfDay()])->selectRaw("id, payment_date, student_id, batch_id, CASE WHEN category='monthly' THEN 'Batch' WHEN category='admission' THEN 'Admission' ELSE remarks END AS category, payment_mode, amount, discount, amount - IFNULL(discount, 0) AS fee")->havingRaw('fee > ?', [0])->get();
         return view('report.fee', compact('inputs', 'branches', 'category', 'fees'));
@@ -110,7 +110,7 @@ class ReportController extends Controller implements HasMiddleware
     function fetchFee(Request $request)
     {
         $inputs = array($request->from_date, $request->to_date, $request->category, $request->branch);
-        $category = array('admission' => 'Admission', 'monthly' => 'Batch', 'all' => 'All');
+        $category = array('admission' => 'Admission', 'monthly' => 'Batch', 'other' => 'Other', 'all' => 'All');
         $branches = $this->branches;
         $fees = Fee::whereBetween('payment_date', [Carbon::parse($inputs[0])->startOfDay(), Carbon::parse($inputs[1])->endOfDay()])->when($request->category != 'all', function ($q) use ($request) {
             return $q->where('category', $request->category);
