d3b4f09a3b4132c48970e8d07df66d016f040b02
Cybernetics <mail@cybernetics.me>
2025-05-16 17:51:58 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -183,7 +183,7 @@ class ReportController extends Controller implements HasMiddleware
     {
         $inputs = array('', Carbon::now()->month, Carbon::now()->year);
         $batches = Batch::where('branch_id', Session::get('branch'))->pluck('name', 'id');
-        $months = Month::pluck('name', 'id');
+        $months = Month::orderBy('id')->pluck('name', 'id');
         $years = Year::pluck('name', 'name');
         $fee = collect();
         return view('report.fee-pending', compact('inputs', 'batches', 'months', 'years', 'fee'));
@@ -198,7 +198,7 @@ class ReportController extends Controller implements HasMiddleware
         ]);
         $inputs = array($request->batch, $request->month, $request->year);
         $batches = Batch::where('branch_id', Session::get('branch'))->pluck('name', 'id');
-        $months = Month::pluck('name', 'id');
+        $months = Month::orderBy('id')->pluck('name', 'id');
         $years = Year::pluck('name', 'name');
         $fee = StudentBatch::leftJoin('batches as b', 'student_batches.batch_id', 'b.id')->selectRaw("student_batches.student_id, b.monthly_fee as fees")->whereNull('student_batches.deleted_at')->where('student_batches.batch_id', $request->batch)->whereNotIn('student_batches.student_id', function ($q) use ($request) {
             return $q->select('student_id')->where('month', $request->month)->where('year', $request->year)->where('batch_id', $request->batch)->where('category', 'monthly')->whereNull('deleted_at')->from('fees');
