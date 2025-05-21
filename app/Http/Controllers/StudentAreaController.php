a484f54a663e72c8270958263216c7c7b4082372
Cybernetics <mail@cybernetics.me>
2025-05-21 16:42:09 +0530

    small updates

--- app/Http/Controllers/StudentAreaController.php
+++ app/Http/Controllers/StudentAreaController.php
@@ -22,7 +22,7 @@ class StudentAreaController extends Controller implements HasMiddleware
 
     function getStudentNotes()
     {
-        $notes = Note::whereIn('topic_id', CourseTopic::whereIn('course_id', Batch::whereIn('id', StudentBatch::where('student_id', Auth::user()->id)->pluck('batch_id'))->pluck('course_id'))->pluck('topic_id'));
+        $notes = Note::whereIn('topic_id', CourseTopic::whereIn('course_id', Batch::whereIn('id', StudentBatch::where('student_id', Auth::user()->id)->pluck('batch_id'))->pluck('course_id'))->pluck('topic_id'))->get();
         dd($notes);
         die;
     }
