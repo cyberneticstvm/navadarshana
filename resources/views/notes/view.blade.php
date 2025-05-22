a6ef79949bf60090528cca7805e1f3d12dd9acf4
Cybernetics <mail@cybernetics.me>
2025-05-22 12:06:04 +0530

    fonts added

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -42,7 +42,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-12 studentNote">
                                 {!! $note->notes !!}
                             </div>
                         </div>
