ceb416e6ab6b1147725bc181db0bfc679c5f0297
Cybernetics <mail@cybernetics.me>
2025-05-22 19:39:20 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -203,7 +203,7 @@ class ReportController extends Controller implements HasMiddleware
         $years = Year::pluck('name', 'name');
         $days = Carbon::now()->month(intval($request->month))->daysInMonth;
         $dt = Carbon::createFromFormat('Y-m-d', $request->year . '-' . $request->month . '-' . $days)->format('Y-m-d');
-        $fee = StudentBatch::leftJoin('batches as b', 'student_batches.batch_id', 'b.id')->leftJoin('students as s', 's.id', 'student_batches.student_id')->where('date_of_admission', '<=', $dt)->selectRaw("student_batches.student_id, b.monthly_fee as fees, s.date_of_admission")->whereNull('student_batches.deleted_at')->where('student_batches.batch_id', $request->batch)->whereNotIn('student_batches.student_id', function ($q) use ($request) {
+        $fee = StudentBatch::leftJoin('batches as b', 'student_batches.batch_id', 'b.id')->leftJoin('students as s', 's.id', 'student_batches.student_id')->whereDate('student_batches.created_at', '<=', $dt)->selectRaw("student_batches.student_id, b.monthly_fee as fees, s.date_of_admission")->whereNull('student_batches.deleted_at')->where('student_batches.batch_id', $request->batch)->whereNotIn('student_batches.student_id', function ($q) use ($request) {
             return $q->select('student_id')->where('month', $request->month)->where('year', $request->year)->where('batch_id', $request->batch)->where('category', 'monthly')->whereNull('deleted_at')->from('fees');
         })->get();
         return view('report.fee-pending', compact('inputs', 'batches', 'months', 'years', 'fee', 'dt'));
