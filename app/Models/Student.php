95249400e2c91b8f3d2f17b7fbb310744de5894a
Cybernetics <mail@cybernetics.me>
2025-05-25 21:41:29 +0530

    small updates

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -46,6 +46,7 @@ class Student extends Model
     public function notes()
     {
         //return Note::whereIn('topic_id', CourseTopic::whereIn('course_id', Course::whereIn('id', Batch::whereIn('id', $this->activeBatches()->pluck('batch_id'))->pluck('course_id')))->pluck('topic_id'));
-        return CourseTopic::whereIn('course_id', Course::whereIn('id', Batch::whereIn('id', $this->activeBatches()->pluck('batch_id'))->pluck('course_id')))->get();
+        $course = Course::whereIn('id', Batch::whereIn('id', $this->activeBatches()->pluck('batch_id'))->pluck('course_id'));
+        return CourseTopic::whereIn('course_id', $course)->get();
     }
 }
