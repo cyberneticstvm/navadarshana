791f88b631b87a054cbb5bdac506eccaba477310
Cybernetics <mail@cybernetics.me>
2025-06-19 10:49:49 +0530

    small updates

--- public/assets/ckeditor5/main.js
+++ public/assets/ckeditor5/main.js
@@ -333,13 +333,13 @@ const {
                 $(this).val(0);
             });
         }
-        $('.optionsContainer').append("<div class='card'><div class='card-header'></div><div class='card-body'><div class='col-md-10'><input type='hidden' name='options[]' value='"+data+"' /><input type='hidden' name='correct_answer' class='correct_answer' value='"+answer+"' />"+data+"</div></div><div class='card-footer text-end'><a href='javascript:void(0)' class='fw-bold text-danger' onclick='$(this).parent().parent().parent().remove()'>Remove</a></div></div>");
-        $('.optionsContainer').find(".card").each(function(){
+        $('.optionsContainer').append("<div class='card'><div class='card-header'></div><div class='card-body'><div class='col-md-10'><input type='hidden' name='options[]' value='"+data+"' /><input type='hidden' name='correct_answer' class='correct_answer' value='"+answer+"' />"+data+"</div></div><div class='card-footer text-end'><a href='javascript:void(0)' class='fw-bold text-danger' onclick='$(this).parent().parent().remove()'>Remove</a></div></div>");
+        $('.optionsContainer').find(".card").each(function(key, el){
             let dis = $(this);
             if(dis.find(".correct_answer").val() == 1){
-                dis.find('.card-header').html('Correct Answer').addClass('text-success');
+                dis.find('.card-header').html('Option' + key + 1).addClass('text-success');
             }else{
-                dis.find('..card-header').html("").removeClass('text-success');
+                dis.find('..card-header').html('Option' + key + 1).removeClass('text-success');
             }
         })        
         editor.setData("");
